164898bb47351297f5c5975c7393c9d5e7065885
19000937 <55851041+19000937@users.noreply.github.com>
2025-05-15 23:00:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')  
 def hello():  
-    return "Hola se termino con exito la parte II!"  
+    return "SSe realizo con exito el laboratorio #4. Si sale este mensaje es que Jenkis y Docker funcionan a la perfeccion. :)!"  
 
 if __name__ == '__main__':  
     app.run(host='0.0.0.0', port=5000)
